34981a741fccd924f0c1d5fb116226d592e79203
Sunny Reitgassl <sunny.reitgassl@stud.th-rosenheim.de>
2022-05-10 15:04:38 +0200

    new version

--- main.py
+++ main.py
@@ -8,16 +8,36 @@ from discord.ext import commands
 from discord import FFmpegPCMAudio
 from youtube_dl import YoutubeDL
 
+intents = discord.Intents.default()
+intents.members = True
+intents.presences = True
+
 TOKEN = "OTI2MDcyODUwMzA4Mjc2MjM0.Yc2WjQ.kgZ0Rfu9h5Pvlkr7loQnzjrfldM"
-client = commands.Bot(command_prefix="!")
-youtube_video_list = [None]
-YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+client = commands.Bot(command_prefix="!", intents=intents)
+youtube_video_list = []
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True', 'outtmpl': 'ytdl/'}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 
 @client.event
 async def on_ready():
     print("Discord bot was started")
+    print(client.is_ws_ratelimited())
+    incelVille = discord.utils.get(client.guilds, id=794272408529797152)
+    print(incelVille.name)
+    general = discord.utils.get(incelVille.channels, id=794272408529797154)
+    print(general.name)
+    pip = incelVille.get_member(385826918844596224)
+    print(pip.name)
+    print(pip.activity)
+    while True:
+        for activity in pip.activities:
+            if activity.name == "League of Legends":
+                print("sent message about pip playing league")
+                await general.send("ATTENTION EVERYONE!\nPIP IS PLAYING LEAGUE!\nTHANK YOU FOR YOUR ATTENTION!")
+                print("sent message about pip playing league")
+                await asyncio.sleep(5)
+        await asyncio.sleep(5)
 
 
 # Makes the bot join the voice channel the user is in
@@ -61,10 +81,10 @@ async def play(ctx, *, search):
     youtube_video_list.append(youtube_video)
 
     # Looping over the youtube_video_list and playing the requests
-    await playMusic(ctx)
+    await play_music(ctx)
 
 
-async def playMusic(ctx):
+async def play_music(ctx):
     while youtube_video_list is not None:
 
         # Gets Video out of playlist
@@ -86,6 +106,8 @@ async def playMusic(ctx):
                 break
             except discord.errors.ClientException:
                 await asyncio.sleep(1)
+            except RuntimeError:
+                return
 
 
 @client.command(pass_context=True, brief="Pauses the song.")
@@ -167,10 +189,45 @@ async def fuckoff(ctx):
 
 
 @client.command(pass_context=True, brief="Puts out a random quote from the quotes channel")
-async def quote(self):
-    quotes_channel = discord.utils.get(self.guild.channels, name="quotes-channel", type=discord.ChannelType.text)
+async def quote(ctx):
+    quotes_channel = discord.utils.get(ctx.guild.channels, name="quotes-channel", type=discord.ChannelType.text)
     messages = await quotes_channel.history(limit=500).flatten()
-    await self.send(random.choice(messages).content)
+    randomMessage = random.choice(messages).content
+    print("nice")
+    await ctx.send(randomMessage)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user or message.channel.name == 'quotes-channel':
+        return
+    wordList = {"chad", "cheese", "vote", "twitter", "surgeon", "deutschland", "germany", ""}
+
+    switcher = {
+        "chad": "https://i.kym-cdn.com/entries/icons/original/000/026/152/gigachad.jpg",
+        "cheese": "https://www.youtube.com/watch?v=y3qrHn0WALs",
+        "vote": "⚠️WARNING⚠️ \ncock inspection is NOT required at the voting booths!!!! don't be tricked like "
+                "me!!\nSo I was waiting in line to vote when all of a sudden this voting \"official\" came up to me "
+                "and said that there was something wrong with my voter registration and asked me to follow him to the "
+                "back. When we went around back he said that I had to take off my pants and show my cock because "
+                "penis size is the most accurate way to confirm voter identity. Because I thought he was a voting "
+                "official I swiftly removed my pants and underwear to show him my member. After he fondled it for a "
+                "bit he said it was good and I could go back into the line. It was only after I voted I realized that "
+                "he forgot to check my balls too!!! He was obviously not certified to check such an area and I "
+                "immediately contacted the security guards about his presence. Please do not fall for any tricks like "
+                "I did! stay safe and happy voting!",
+        "twitter": "OFFICIAL ACCOUNT!\nBLM, ACAB\nshe/her•13 💁♥️ • 2-9-07 💋💞 • Atheist 😈💫 • God's Princess 😇🙏• "
+                   "Twerker 😵🍑 • aquarius ♒• DON'T MESSED WITH ME BITCH 😏✨ • Jake 😍💕",
+        "surgeon": "In Korea, heart surgeon. Number one. Steady hand. One day, Kim Jong Un need new heart. I do "
+                   "operation. But mistake! Kim Jong Un die! SSD very mad! I hide fishing boat, come to America. No "
+                   "English, no food, no money. Darryl give me job. Now I have house, American car and new woman. "
+                   "Darryl save life.\nMy big secret. I kill Kim Jong Un on purpose. I good surgeon. The best!",
+        "deutschland": "Ein Land, ein Reich, ein Kommentarbereich!",
+        "germany": "Ein Land, ein Reich, ein Kommentarbereich!",
+        "fortnite": "https://tenor.com/view/fortnite-default-dance-gif-13330926",
+        "69": "nice"
+    }
+    await message.channel.send(switcher.get(message.content.lower()))
 
 
 client.run(TOKEN)
